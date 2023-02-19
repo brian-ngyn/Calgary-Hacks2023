b9053bdaa0c3b84c93876eba3184b44bdbf3e67d
Dimitar Janevski <dimitar.j27@gmail.com>
2023-02-18 18:34:40 -0700

    add sign out

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -5,7 +5,7 @@ import { useUserAuth } from "../authentication/UserAuthContext";
 
 function Navbar() {
   const location = useLocation();
-  const { user, docSnap, googleSignIn } = useUserAuth();
+  const { user, docSnap, googleSignIn, logout } = useUserAuth();
 
   const handleGoogleSignIn = async (e) => {
     e.preventDefault();
@@ -15,6 +15,14 @@ function Navbar() {
       setError(error.message);
     }
   };
+
+  const handleSignOut = async (e) => {
+    try {
+      await logout();
+    } catch (error) {
+      setError(error.message);
+    }
+  }
 
   return (
     <>
@@ -27,22 +35,12 @@ function Navbar() {
           </IconButton>
           {(docSnap && !docSnap.new_sign_up) ? (
             <div className="group flex space-x-5 self-end pb-4 transition-all duration-300 ease-in-out">
-              <Link
-                className={location.pathname == "/page1" ?
-                  "underline underline-offset-[4px] decoration-2 " :
-                  `hover:before:scale-x-100 hover:before:origin-left relative before:w-full before:h-0.5 before:origin-right before:transition-transform before:duration-300 before:scale-x-0 before:bg-[#545454] before:absolute before:left-0 before:bottom-0`}
-                to="/page1"
+              <div
+                className="text-l text-black hover:cursor-pointer hover:before:scale-x-100 hover:before:origin-left relative before:w-full before:h-0.5 before:origin-right before:transition-transform before:duration-300 before:scale-x-0 before:bg-[#545454] before:absolute before:left-0 before:bottom-0"
+                onClick={(e) => logout(e)}
               >
-                Page 1
-              </Link>
-              <Link
-                className={location.pathname == "/page2" ?
-                  "underline underline-offset-[4px] decoration-2 " :
-                  "hover:before:scale-x-100 hover:before:origin-left relative before:w-full before:h-0.5 before:origin-right before:transition-transform before:duration-300 before:scale-x-0 before:bg-[#545454] before:absolute before:left-0 before:bottom-0 "}
-                to="/page2"
-              >
-                Page 2
-              </Link>
+                Sign Out
+              </div>
             </div>
           ) : (<div className="group flex space-x-5 self-end pb-4 transition-all duration-300 ease-in-out">
             <div

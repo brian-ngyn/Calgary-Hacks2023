21fb4f8bfffea24bd7bc5c4495b310a262e68f54
topanb11 <topanbudiman11@gmail.com>
2023-02-19 10:41:54 -0700

    Changed portfolio to grid

--- frontend/src/components/PortfolioCard.jsx
+++ frontend/src/components/PortfolioCard.jsx
@@ -5,8 +5,8 @@ function PortfolioCard() {
 	console.log(docSnap)
 
 	return ( 
-		<div className="shadow-md w-[280px] h-[280px] bg-[#FFFFFF] flex flex-col justify-center items-center rounded-2xl">
-			<img className="w-[240px] h-[180px] mb-5" src={""}/>
+		<div className="shadow-md w-[250px] h-[280px] bg-[#FFFFFF] flex flex-col justify-center items-center rounded-2xl">
+			<img className="w-[200px] h-[180px] mb-5" src={""}/>
 			<h2>SPORT</h2>
 		</div>		
 	 );
